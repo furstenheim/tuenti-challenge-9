e0b47fd13f9c059b33bf7d2c6c04ded003d00c3c
gabriel <furstenheim@gmail.com>
2019-05-02 10:22:46 +0200

    Convert number to kanji

--- 09/main.go
+++ 09/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "log"
 
 type Case struct {
 	op1, op2, result KanjiNumber
@@ -15,12 +16,90 @@ type ProcessedNumber struct {
 	raw            KanjiNumber
 	frequencyCount FrequencyMap
 }
+type PowerOfTen struct {
+	hasRune, listsUnit bool
+	rune               rune
+}
+const MAX_NUMBER = 99999
+const MIN_NUMBER = 1
+var powersOfTen = []PowerOfTen{
+	{
+		hasRune: false,
+		listsUnit: true,
+		rune: 'x',
+	},
+	{
+		hasRune: true,
+		listsUnit: false,
+		rune: '十',
+	},
+	{
+		hasRune: true,
+		listsUnit: false,
+		rune: '百',
+	},
+	{
+		hasRune: true,
+		listsUnit: false,
+		rune: '千',
+	},
+	{
+		hasRune: true,
+		listsUnit: true,
+		rune: '万',
+	},
+}
+
+var digitToKanji = map[int]rune {
+	1: '一',
+	2: '二',
+	3: '三',
+	4: '四',
+	5: '五',
+	6: '六',
+	7: '七',
+	8: '八',
+	9: '九',
+}
 
 /*
 func toRawNumber (n int) KanjiNumber {
 
 }
 */
+
+func intToKanji (n int) KanjiNumber {
+	if n > MAX_NUMBER {
+		log.Fatal("Number too big", n)
+	}
+	if n < MIN_NUMBER {
+		log.Fatal("Negative number", n)
+	}
+	kanjiNumber := make(KanjiNumber, 0)
+	for _, p := range(powersOfTen) {
+		v := n % 10
+		n = n / 10
+		if v == 0 {
+			continue
+		}
+		k, ok := digitToKanji[v]
+		if !ok {
+			log.Fatal("Unknown digit", k)
+		}
+		if !p.hasRune {
+			kanjiNumber = append(kanjiNumber, k)
+		} else if (!p.listsUnit && v == 1) {
+			kanjiNumber = append(kanjiNumber, p.rune)
+		} else {
+			kanjiNumber = append(kanjiNumber, p.rune, k)
+		}
+
+	}
+	kanjiNumber.reverse()
+	return kanjiNumber
+
+}
+
 
 func (m FrequencyMap) isKanjiPossible (n KanjiNumber) bool {
 	mapCopy := m.clone()
@@ -52,4 +131,9 @@ func (m FrequencyMap) clone () FrequencyMap {
 
 
 
-
+func (k KanjiNumber) reverse () {
+	for i := len(k)/2-1; i >= 0; i-- {
+		opp := len(k)-1-i
+		k[i], k[opp] = k[opp], k[i]
+	}
+}
--- 09/main_test.go
+++ 09/main_test.go
@@ -25,6 +25,12 @@ func TestCaseIsKanjiPossible (t *testing.T) {
 			expected: false,
 		},
 		{
+			n: KanjiNumber{'二', '二'},
+			m: FrequencyMap{'二': 1},
+			probeRune: '二',
+			expected: false,
+		},
+		{
 			n: KanjiNumber{'十', '二'},
 			m: FrequencyMap{'二': 2},
 			probeRune: '二',
@@ -41,5 +47,45 @@ func TestCaseIsKanjiPossible (t *testing.T) {
 		isPossible := tc.m.isKanjiPossible(tc.n)
 		assert.Equal(t, tc.expected, isPossible)
 		assert.GreaterOrEqual(t, tc.m[tc.probeRune], 1) // check we cloned correctly
+	}
+}
+func TestNumberToKanji (t *testing.T) {
+	testCases := []struct{
+		n int
+		expected KanjiNumber
+	}{
+		{
+			n: 2,
+			expected: KanjiNumber{'二'},
+		},
+		{
+			n: 1,
+			expected: KanjiNumber{'一'},
+		},
+		{
+			n: 10,
+			expected: KanjiNumber{'十'},
+		},
+		{
+			n: 12,
+			expected: KanjiNumber{'十', '二'},
+		},
+		{
+			n: 121,
+			expected: KanjiNumber{'百', '二', '十', '一'},
+		},
+		{
+			n: 1345,
+			expected: KanjiNumber{'千', '三', '百', '四', '十', '五'},
+		},
+		{
+			n: 12345,
+			expected: KanjiNumber{'一', '万', '二', '千', '三', '百', '四', '十', '五'},
+		},
+
+	}
+	for _, tc := range(testCases) {
+		expected := intToKanji(tc.n)
+		assert.Equal(t, tc.expected, expected)
 	}
 }
